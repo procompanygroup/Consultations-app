995c708f2f3c3dc2d9c2bcd417e5f048f367cb96
git-dina <71346398+git-dina@users.noreply.github.com>
2024-01-19 00:02:15 +0300

    Update global.dart

--- rouh_app/lib/constants/global.dart
+++ rouh_app/lib/constants/global.dart
@@ -1,5 +1,5 @@
 
  const String accountSid = 'ACd0dac3942d72e08c1b0a4e1eceae8d4f'; // replace it with your account SID
- const String authToken = 'c1be50fb1aec56cbaf06ff5a966b7ab1'; // replace it with your auth token
+ const String authToken = '182d7563e40ace93b7540fab821af005'; // replace it with your auth token
  const String twilioNumber = '+12062021682'; // replace it with your purchased twilioNumber
  const String smsBody ='Hello This is 6 digit code to verify phone';
