fcb6bcd73bd64cb6ad577b26a0821383361f072f
yasinidlbi <yasinidlbi@gmail.com>
2024-01-25 19:12:41 +0300

    Update main_navigation_screen.dart

--- rouh_app/lib/screens/main_navigation_screen.dart
+++ rouh_app/lib/screens/main_navigation_screen.dart
@@ -20,6 +20,9 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     const List<Widget> _pages = <Widget>[
       ServiceScreen(),
       Text(
@@ -43,33 +46,40 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
       body: Center(
         child: _pages.elementAt(_selectedIndex), //New
       ),
-      bottomNavigationBar: ClipRRect(
-        borderRadius: BorderRadius.only(topLeft: Radius.circular(35.0),topRight: Radius.circular(35.0)),
-        child: BottomNavigationBar(
-          //backgroundColor: Colors.red,
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xff015DAC),Color(0xff015DAC)],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter  ,
+            )
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(topLeft: Radius.circular(35.0),topRight: Radius.circular(35.0)),
+          child: BottomNavigationBar(
+            currentIndex: _selectedIndex,
+            onTap: _onItemTapped,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home,size: 35),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle,size: 35) ,
+                label: 'sercice',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.people,size: 35) ,
+                label: 'experts',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.people,size: 35) ,
+                label: 'profile',
+              ),
+            ],
+            type: BottomNavigationBarType.fixed,
 
-          currentIndex: _selectedIndex,
-          onTap: _onItemTapped,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home,size: 35),
-              label: 'home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.account_circle,size: 35) ,
-              label: 'sercice',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.people,size: 35) ,
-              label: 'experts',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.people,size: 35) ,
-              label: 'profile',
-            ),
-          ],
-          type: BottomNavigationBarType.fixed,
-
+          ),
         ),
       ),
     );
