12d58cdb6d163b4c275a54ef87ed6123957bec2b
git-dina <71346398+git-dina@users.noreply.github.com>
2024-01-26 17:36:42 +0300

    Update service_model.dart

--- rouh_app/lib/models/service_model.dart
+++ rouh_app/lib/models/service_model.dart
@@ -14,15 +14,15 @@ class AppService {
 
   //Constructor
   AppService(
-      { name, desc, icon, is_active, image,}) {
-    name = name;
-    desc = desc;
-    icon = icon;
-    is_active = is_active;
-    image = image;
+      { this.name, this.desc, this.icon, this.is_active, this.image,}) {
+    // name = name;
+    // desc = desc;
+    // icon = icon;
+    // is_active = is_active;
+    // image = image;
   }
 
-  factory AppService.fromJson(Map<String, dynamic> parsedJson) {
+  factory AppService.fromJson(dynamic parsedJson) {
     return AppService(
         name: parsedJson['name'],
         desc: parsedJson['desc'],
@@ -45,14 +45,18 @@ class AppService {
 
     var response = await dioManager.dio.post('client/service/viewall', );
     if (response.statusCode == 200) {
-      Iterable l = json.decode(response.data);
-      List<AppService> services = List<AppService>.from(l.map((model)=> AppService.fromJson(model)));
-
+      var services = convertListToModel<AppService>(AppService.fromJson,jsonDecode(response.data));
       return services;
     }
     else {
       return throw Exception();
     }
 
+  }
+
+// used  for convert a List of value
+  List<T> convertListToModel<T>(
+      T Function(Map<String, dynamic> map) fromJson, List data) {
+    return data.map((e) => fromJson((e as Map).cast())).toList();
   }
 }
