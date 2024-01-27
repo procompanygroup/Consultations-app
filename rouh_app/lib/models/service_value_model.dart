3df6e93cf21810b4107c2ed1e58773317b29dede
git-dina <71346398+git-dina@users.noreply.github.com>
2024-01-27 21:48:04 +0300

    Create service_value_model.dart

---
+++ rouh_app/lib/models/service_value_model.dart
@@ -0,0 +1,24 @@
+import 'package:rouh_app/models/service_input_model.dart';
+
+class ServiceValue {
+  //Instance variables
+  int? id;
+  String? value;
+  int? selectedservice_id;
+  int? inputservice_id;
+
+
+  //Constructor
+  ServiceValue({ this.id, this.value, this.selectedservice_id, this.inputservice_id });
+
+  Future<List<ServiceValue>> generateInputValues({required List<ServiceInput>? serviceInputs,required int serviceId}) async {
+   List<ServiceValue> serviceValues=[];
+    for (var i = 0; i < serviceInputs!.length; i++) {
+      serviceValues.add( ServiceValue( inputservice_id:serviceInputs[i].id,selectedservice_id: serviceId));
+    }
+  return serviceValues;
+    
+    }
+
+
+}
