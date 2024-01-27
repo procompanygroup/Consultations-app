f30bc52a1a5c3de05267637bd484a5653767786b
git-dina <71346398+git-dina@users.noreply.github.com>
2024-01-27 15:01:04 +0300

    Update service_model.dart

--- rouh_app/lib/models/service_model.dart
+++ rouh_app/lib/models/service_model.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:rouh_app/models/service_input_model.dart';
+
 import '../controllers/dio_manager_controller.dart';
 
 class AppService {
@@ -9,12 +11,13 @@ class AppService {
   String? icon;
   int? is_active;
   String? image;
+  List<ServiceInput>? serviceInputs;
 
   DioManager dioManager = DioManager();
 
   //Constructor
   AppService(
-      { this.name, this.desc, this.icon, this.is_active, this.image,}) {
+      { this.name, this.desc, this.icon, this.is_active, this.image,this.serviceInputs}) {
     // name = name;
     // desc = desc;
     // icon = icon;
@@ -24,12 +27,13 @@ class AppService {
 
   factory AppService.fromJson(dynamic parsedJson) {
     return AppService(
-        name: parsedJson['name'],
-        desc: parsedJson['desc'],
-        icon: parsedJson['icon'],
-        image: parsedJson['image'],
-        is_active: parsedJson['is_active'],
-    );
+    name: parsedJson['name'],
+    desc: parsedJson['desc'],
+    icon: parsedJson['icon'],
+    image: parsedJson['image'],
+     is_active: parsedJson['is_active'],
+     serviceInputs : convertListToModel(ServiceInput.fromJson, parsedJson["inputservices"]),
+     );
   }
 
   Map<String, dynamic> toJson() =>
@@ -54,8 +58,28 @@ class AppService {
 
   }
 
+  Future<AppService?> getSrviceInputs({
+    required int serviceId,
+  }) async {
+
+    var data = json.encode({
+      "id": serviceId
+    });
+
+    var response = await dioManager.dio.post('client/service/getinputform',data: data );
+    if (response.statusCode == 200) {
+      var service = AppService.fromJson(json.decode(response.data));
+
+      return service;
+    }
+    else {
+      return throw Exception();
+    }
+
+  }
+
 // used  for convert a List of value
-  List<T> convertListToModel<T>(
+ static List<T> convertListToModel<T>(
       T Function(Map<String, dynamic> map) fromJson, List data) {
     return data.map((e) => fromJson((e as Map).cast())).toList();
   }
