9709fd3f565d37384be4d0220020c289419fc394
RaghadKatbi <137506665+RaghadKatbi@users.noreply.github.com>
2024-01-28 13:17:06 +0300

    update user_information_cubit

--- rouh_app/lib/bloc/UserInformation/user_information_cubit.dart
+++ rouh_app/lib/bloc/UserInformation/user_information_cubit.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
@@ -11,24 +9,16 @@ part 'user_information_state.dart';
 class UserInformationCubit extends Cubit<UserInformationState> {
   UserInformationCubit() : super(UserInformationInitial());
   DioManager dioManager = DioManager();
-  Future<User?> getUser({
-    required String mobile,
-  }) async {
-    var data = json.encode({"mobile": mobile});
 
-    var response = await dioManager.dio.post(
-      'client/getbymobile',
-      data: data,
-    );
-    print(response.statusMessage);
+  Future<User?> getUserInformation({required mobile}) {
+    User infoUser = User();
     emit(UserInformationLoading());
-    if (response.statusCode == 200) {
-      emit(UserInformationSuccess());
-      // return User.fromJson(json.decode(response.data));
-      return User.fromJson(response.data);
-    } else {
+    var user = infoUser.getUser(mobile: mobile);
+    if (user == Exception()) {
       emit(UserInformationFailure());
-      return throw Exception();
+    } else {
+      emit(UserInformationSuccess());
     }
+    return user;
   }
 }
