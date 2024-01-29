a70958ee4221e1668670310ce9df7c84a617605e
yasinidlbi <yasinidlbi@gmail.com>
2024-01-29 17:05:06 +0300

    Update service_screen.dart

--- rouh_app/lib/screens/service/service_screen.dart
+++ rouh_app/lib/screens/service/service_screen.dart
@@ -24,30 +24,12 @@ class _ServiceScreenState extends State<ServiceScreen> {
   Service appService = Service();
   List<Service> serviceList = <Service>[];
 
-/*
-  List<Service> serviceList = [
+  List<Service> starterServiceList = [
     Service(
-        name: "طاقة الشفاء",
-        desc: "desc",
-        image: "https://picsum.photos/200/300?random=1"),
-    Service(
-        name: "Service_2",
-        desc: "desc",
-        image: "https://picsum.photos/200/300?random=2"),
-    Service(
-        name: "Service_3",
-        desc: "desc",
-        image: "https://picsum.photos/200/300?random=3"),
-    Service(
-        name: "Service_4",
-        desc: "desc",
-        image: "https://picsum.photos/200/300?random=4"),
-    Service(
-        name: "Service_5",
-        desc: "desc",
-        image: "https://picsum.photos/200/300?random=5"),
+        name: "",
+        desc: "",
+        image: ""),
   ];
-*/
 
   @override
   void initState() {
@@ -59,40 +41,158 @@ class _ServiceScreenState extends State<ServiceScreen> {
   }
 
   Future<void> fillServiceList() async {
-     print(isLoading);
-     //print( await appService.allServices());
-      serviceList = await appService.allServices();
+
+    print("Start:" + isLoading.toString());
+    // serviceList = await appService.allServices();
+      appService.allServices()
+        .then((response) {
+      setState(() {
+        print(response.length);
+       serviceList =response;
+        isLoading =false;
+        print("Test:" + isLoading.toString());
+
+      });
+    });
+    print(serviceList);
+    print("End:" + isLoading.toString());
+
+
+      /*
      for (var i = 0; i < serviceList.length; i++) {
        print(serviceList[i].name);
        print(serviceList[i].desc);
        print(serviceList[i].image);
      }
-     
+     */
+     /*
      var serviceInput = await appService.getServiceInputs(serviceId: 1);
      print(serviceInput!.serviceInputs![0].id);
-
+*/
+    /*
      ServiceValue serviceValue = ServiceValue();
      var serviceVlues =await serviceValue.generateInputValues(serviceInputs:serviceInput.serviceInputs);
      print(serviceVlues![0].inputservice_id);
-
-     isLoading =false;
-     print(isLoading);
-/*
-
-     print("Start:" + isLoading.toString());
-    appService.allServices()
-        .then((response) {
-      setState(() {
-        print(response);
-        // serviceList =response;
-        isLoading =false;
-        print("Test:" + isLoading.toString());
-
-      });
-      });
-     print(serviceList);
-     print("End:" + isLoading.toString());
 */
+    /*
+
+         print("Start:" + isLoading.toString());
+        appService.allServices()
+            .then((response) {
+          setState(() {
+            print(response);
+            // serviceList =response;
+            isLoading =false;
+            print("Test:" + isLoading.toString());
+
+          });
+          });
+         print(serviceList);
+         print("End:" + isLoading.toString());
+    */
+  }
+
+  Widget buildPages(List<Service> _serviceList)
+  {
+    var pages = List.generate(
+        _serviceList.length,
+            (index) => Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(color: Colors.white, width: 0.3),
+            color: Colors.white,
+          ),
+          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 4),
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  Expanded(
+                    child: Container(
+                      width: double.maxFinite,
+                      // height: height,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image(
+                          image: NetworkImage(_serviceList[index].image!),
+                          fit: BoxFit.fitHeight,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 15),
+                    child: Text(
+                      _serviceList[index].name!,
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: myprimercolor,
+                        // fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Positioned(
+                right: 5,
+                bottom: 35,
+                child: Container(
+                  //height: MediaQuery.of(context).size.height * 0.65,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(25)),
+                    color: Colors.white,
+                    border: Border.all(color: Colors.grey.shade200),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.03),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: Icon(Icons.favorite,
+                      color: mysecondarycolor,
+                      size: 30,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
+
+    return Column(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: PageView.builder(
+              controller: pageController,
+              // itemCount: pages.length,
+              itemBuilder: (_, index) {
+                return pages[index % pages.length];
+              },
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: SmoothPageIndicator(
+            controller: pageController,
+            count: pages.length,
+            effect: const WormEffect(
+              activeDotColor: mysecondarycolor,
+              dotHeight: 10,
+              dotWidth: 10,
+              type: WormType.thinUnderground,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 
   @override
@@ -102,96 +202,11 @@ class _ServiceScreenState extends State<ServiceScreen> {
     double bodyHeight = (MediaQuery.of(context).size.height //screen
             -MediaQuery.of(context).padding.top // safe area
             -AppBar().preferredSize.height //AppBar
-        - 20 // padding bottom
+        // - 20 // padding bottom
         );
 
-    final pages = List.generate(
-        serviceList.length,
-        (index) => Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                border: Border.all(color: Colors.white, width: 0.3),
-                color: Colors.white,
-              ),
-              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 4),
-              child: Stack(
-                children: [
-                  Column(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          width: double.maxFinite,
-                          // height: height,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(20),
-                            child: Image(
-                              image: NetworkImage(serviceList[index].image!),
-                              fit: BoxFit.fitWidth,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 15),
-                        child: Text(
-                          serviceList[index].name!,
-                          style: TextStyle(
-                              fontSize: 18,
-                              color: myprimercolor,
-                              // fontWeight: FontWeight.bold
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                  Positioned(
-                    right: 5,
-                    bottom: 35,
-                    child: Container(
-                      //height: MediaQuery.of(context).size.height * 0.65,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(25)),
-                        color: Colors.white,
-                        border: Border.all(color: Colors.grey.shade200),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.03),
-                            spreadRadius: 5,
-                            blurRadius: 7,
-                            offset: Offset(0, 3), // changes position of shadow
-                          ),
-                        ],
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(5),
-                        child: Icon(Icons.favorite,
-                          color: mysecondarycolor,
-                          size: 30,
-                        ),
-                      ),
-                    ),
-                    /*
-                    child: ElevatedButton(
-                      child: Icon( serviceList[index].favorite!? Icons.favorite : Icons.favorite_border,
-                        color: mysecondarycolor
-                        , size: 35,),
-                      style: ElevatedButton.styleFrom(
-                        shape: CircleBorder(),
-                        padding: EdgeInsets.all(5),
-                        backgroundColor: Colors.white, // <-- Button color
-                        // foregroundColor: Colors.red, // <-- Splash color
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          serviceList[index].favorite = serviceList[index].favorite!? false: true;
-                        });
-                      },
-                    ),
-                    */
-                  ),
-                ],
-              ),
-            ));
+
+
 
     return Scaffold(
       // appBar: AppBar(
@@ -221,12 +236,12 @@ class _ServiceScreenState extends State<ServiceScreen> {
             ),
 
           ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 20),
-            child: SafeArea(
-              child: Column(
-                children: <Widget>[
-                  Stack(
+          SafeArea(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: (bodyHeight * 0.25),
+                  child: Stack(
                     children: [
                       // Top
                       Container(
@@ -269,45 +284,21 @@ class _ServiceScreenState extends State<ServiceScreen> {
                       )
                     ],
                   ),
-                  !isLoading?
-                  Column(
-                    children: [
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: PageView.builder(
-                            controller: pageController,
-                            // itemCount: pages.length,
-                            itemBuilder: (_, index) {
-                              return pages[index % pages.length];
-                            },
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10),
-                        child: SmoothPageIndicator(
-                          controller: pageController,
-                          count: pages.length,
-                          effect: const WormEffect(
-                            activeDotColor: mysecondarycolor,
-                            dotHeight: 10,
-                            dotWidth: 10,
-                            type: WormType.thinUnderground,
-                          ),
-                        ),
-                      ),
-                    ],
-                   )
-                  :Padding(
-                    padding:  EdgeInsets.only(top: bodyHeight *0.3),
-                    child: Container(
-                      color: Colors.red,
-                      child: Center(child: Text("Hello"),),
-                    ),
-                  )
-                ],
-              ),
+                ),
+                !isLoading?
+                Container(
+                  height: (bodyHeight * 0.70),
+                    child: buildPages(serviceList))
+                :
+                Container(
+                    height: (bodyHeight * 0.70),
+                    child: Stack(
+                      children: [
+                        buildPages(starterServiceList),
+                        Center(child: CircularProgressIndicator()),
+                      ],
+                    )),
+              ],
             ),
           ),
         ],
