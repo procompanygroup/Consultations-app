26a784b5b1a0870e3ee3530fbb1ecd1ac688e43b
yasinidlbi <yasinidlbi@gmail.com>
2024-02-06 17:18:57 +0300

    Update global.dart

--- rouh_app/lib/constants/global.dart
+++ rouh_app/lib/constants/global.dart
@@ -1,7 +1,14 @@
 // API
+import '../models/service_model.dart';
+
 const String baseUrl = 'https://oras.orasweb.com/api/';
 
+// Sms Settings
  const String accountSid = 'ACd0dac3942d72e08c1b0a4e1eceae8d4f'; // replace it with your account SID
  const String authToken = 'f39dc96e430bf32a85e8c365b3f449cd'; // replace it with your auth token
  const String twilioNumber = '+12062021682'; // replace it with your purchased twilioNumber
  const String smsBody ='Hello This is 6 digit code to verify phone';
+
+
+
+
