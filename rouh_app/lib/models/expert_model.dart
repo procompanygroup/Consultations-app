eab2461065be0cc585c2bb83d59315e41cd12b49
git-dina <71346398+git-dina@users.noreply.github.com>
2024-02-19 18:07:06 +0300

    expert comments

--- rouh_app/lib/models/expert_model.dart
+++ rouh_app/lib/models/expert_model.dart
@@ -3,11 +3,14 @@ import 'dart:convert';
 import '../controllers/dio_manager_controller.dart';
 import 'expert_service_model.dart';
 import 'service_model.dart';
+import 'comment_model.dart';
 
 class Expert {
   //Instance variables
   int? id;
   String? expert_name;
+  String? first_name;
+  String? last_name;
   String? email;
   String? mobile;
   String? nationality;
@@ -25,20 +28,30 @@ class Expert {
   double? answer_speed;
   String? image;
   bool? isFavorite;
-List<ExpertService>? expert_services;
-List<Service>? services;
-
+  List<ExpertService>? expert_services;
+  List<Service>? services;
+  List<ExpertComment>? selectedServices;
   DioManager dioManager = DioManager();
 
   //Constructor
   Expert(
-      { this.id, this.expert_name, this.email, this.mobile, this.nationality, this.birthdate, this.gender, this.marital_status,
+      { this.id, this.expert_name,
+        this.first_name,
+        this.last_name,
+        this.email, this.mobile, this.nationality, this.birthdate, this.gender, this.marital_status,
         this.is_active,this.points_balance,this.cash_balance,this.cash_balance_todate,this.rates,this.record,this.desc,
-        this.call_cost,this.answer_speed, this.image,this.isFavorite, this.expert_services,this.services});
+        this.call_cost,this.answer_speed, this.image,this.isFavorite, this.expert_services,this.services,this.selectedServices});
 
   factory Expert.fromJson(Map<String, dynamic> parsedJson) {
     var tmpExpertServices;
     var tmpServices;
+    var tmpSelectedServices;
+    var tmpBirthDate;
+    var tmpCashBalance;
+    var tmpCashBalanceTodate;
+    var tmpRates;
+    var tmpFav;
+
     if(parsedJson["experts_services"] != null)
     {
       tmpExpertServices = convertListToModel(ExpertService.fromJson, parsedJson["experts_services"]);
@@ -47,28 +60,52 @@ List<Service>? services;
     {
       tmpServices = convertListToModel(Service.fromJson, parsedJson["services"]);
     }
+    if(parsedJson["selectedservices"] != null)
+    {
+      tmpSelectedServices = convertListToModel(ExpertComment.fromJson, parsedJson["selectedservices"]);
+    }
+    if(parsedJson["birthdate"] != null)
+    {
+      tmpBirthDate = DateTime.tryParse(parsedJson['birthdate']);
+    }
+    if(parsedJson['cash_balance'] != null) {
+      tmpCashBalance = double.tryParse( parsedJson['cash_balance']);
+    }
+    if(parsedJson['cash_balance_todate'] != null) {
+      tmpCashBalanceTodate = double.tryParse( parsedJson['cash_balance_todate']);
+    }
+    if(parsedJson['rates'] != null) {
+      tmpRates = double.tryParse( parsedJson['rates']);
+    }
+    if(parsedJson['is_favorite'] != null) {
+      tmpFav = bool.tryParse( parsedJson['is_favorite'].toString());
+    }
+
     return Expert(
-        id: parsedJson['id'],
-        expert_name: parsedJson['user_name'],
-        mobile: parsedJson['mobile'],
-        birthdate: DateTime.tryParse(parsedJson['birthdate']),
-        email: parsedJson['email'],
-        gender: parsedJson['gender'],
-        image: parsedJson['image'],
-        is_active: parsedJson['is_active'],
-        marital_status: parsedJson['marital_status'],
-        nationality: parsedJson['nationality'],
+      id: parsedJson['id'],
+      expert_name: parsedJson['user_name'],
+      first_name: parsedJson['first_name'],
+      last_name: parsedJson['last_name'],
+      mobile: parsedJson['mobile'],
+      birthdate: tmpBirthDate,
+      email: parsedJson['email'],
+      gender: parsedJson['gender'],
+      image: parsedJson['image'],
+      is_active: parsedJson['is_active'],
+      marital_status: parsedJson['marital_status'],
+      nationality: parsedJson['nationality'],
       call_cost: parsedJson['call_cost'],
       answer_speed: parsedJson['answer_speed'],
-      cash_balance:double.tryParse( parsedJson['cash_balance']),
-      cash_balance_todate: double.tryParse(parsedJson['cash_balance_todate']),
+      cash_balance:tmpCashBalance,
+      cash_balance_todate: tmpCashBalanceTodate,
       desc:  parsedJson['desc'],
       record:  parsedJson['record'],
-       points_balance:  parsedJson['points_balance'],
-      rates:  double.tryParse(parsedJson['rates']),
-      isFavorite:  bool.tryParse(parsedJson['is_favorite']),
+      points_balance:  parsedJson['points_balance'],
+      rates:  tmpRates,
+      isFavorite: tmpFav,
       expert_services: tmpExpertServices,
       services: tmpServices,
+      selectedServices: tmpSelectedServices,
     );
   }
 
@@ -96,7 +133,7 @@ List<Service>? services;
 
 
 
-Future<List<Expert>> GetWithFavorite({
+  Future<List<Expert>> GetWithFavorite({
     required int clientId,
   }) async {
     var data = json.encode({
@@ -108,7 +145,9 @@ Future<List<Expert>> GetWithFavorite({
     );
 
     List<Expert> experts;
+    print(response.statusCode);
     if (response.statusCode == 200) {
+      print((response.data));
       experts = convertListToModel<Expert>(Expert.fromJson,jsonDecode(response.data));
 
     }
@@ -117,10 +156,11 @@ Future<List<Expert>> GetWithFavorite({
     }
     return experts;
   }
-Future<bool> SaveFavorite({
+
+  Future<bool> SaveFavorite({
     required int clientId,
-  required int expertId,
-  required bool isFavorite
+    required int expertId,
+    required bool isFavorite
   }) async {
     var data = json.encode({
       "client_id": clientId,
@@ -133,7 +173,7 @@ Future<bool> SaveFavorite({
     );
 
     if (response.statusCode == 200) {
-     return true;
+      return true;
 
     }
     else {
@@ -141,6 +181,26 @@ Future<bool> SaveFavorite({
     }
   }
 
+  Future<Expert> GetExpertComments({
+    required int expertId,
+  }) async {
+    var data = json.encode({
+      "id": expertId
+    });
+
+    var response = await dioManager.dio.post('client/expert/getwithcomments',
+      data: data,
+    );
+
+    List<Expert> experts;
+    if (response.statusCode == 200) {
+      return Expert.fromJson(jsonDecode(response.data));
+
+    }
+    else {
+      return throw Exception();
+    }
+  }
   // used  for convert a List of value
   static List<T> convertListToModel<T>(
       T Function(Map<String, dynamic> map) fromJson, List data) {
