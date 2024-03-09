6bf58e455159a8fdcdee965d14d9bf2b26bc8cc3
yasinidlbi <yasinidlbi@gmail.com>
2024-03-09 23:37:25 +0300

    .

--- rouh_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ rouh_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,16 @@
 import FlutterMacOS
 import Foundation
 
-import audioplayers
+import audioplayers_darwin
 import file_selector_macos
 import flutter_secure_storage_macos
 import path_provider_foundation
+import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AudioplayersPlugin.register(with: registry.registrar(forPlugin: "AudioplayersPlugin"))
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
