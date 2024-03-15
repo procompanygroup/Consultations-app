cd359412cb5f94ca9a9c960d6e17164dccf2804e
yasinidlbi <yasinidlbi@gmail.com>
2024-03-15 21:49:54 +0300

    Update GeneratedPluginRegistrant.swift

--- rouh_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ rouh_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,6 @@ import Foundation
 
 import audioplayers_darwin
 import file_selector_macos
-import firebase_core
-import firebase_messaging
 import flutter_local_notifications
 import flutter_secure_storage_macos
 import path_provider_foundation
@@ -17,8 +15,6 @@ import sqflite
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
