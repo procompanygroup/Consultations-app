cfaa09884c17b84d6b46dc604b0f872859d480ef
yasinidlbi <yasinidlbi@gmail.com>
2024-03-16 21:21:26 +0300

    add stream

--- rouh_app/lib/Services/notifi_service.dart
+++ rouh_app/lib/Services/notifi_service.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+import '../controllers/globalController.dart';
 
 class NotificationService {
   final FlutterLocalNotificationsPlugin notificationsPlugin =
@@ -15,7 +19,16 @@ class NotificationService {
       requestBadgePermission: true,
       requestSoundPermission: true,
       onDidReceiveLocalNotification:
-      (int id, String? title, String? body, String? payload) async {});
+      (int id, String? title, String? body, String? payload) async {
+        didReceiveLocalNotificationStream.add(
+          ReceivedNotification(
+            id: id,
+            title: title,
+            body: body,
+            payload: payload,
+          ),
+        );
+      });
 
     var initializationSettings = InitializationSettings(
       android: initializationSettingsAndroid,
@@ -58,11 +71,65 @@ class NotificationService {
       print('notification payload: ' + notificationResponse.id!.toString());
       print('notification payload: ' + notificationResponse.payload!);
 
+
+      switch (notificationResponse.notificationResponseType) {
+        case NotificationResponseType.selectedNotification:
+          selectNotificationStream.add(notificationResponse.payload);
+          break;
+        case NotificationResponseType.selectedNotificationAction:
+          if (notificationResponse.actionId == '0') {
+            selectNotificationStream.add(notificationResponse.payload);
+          }
+          break;
+      }
+
       //
       // await Navigator.push(
       //   context,
       //   MaterialPageRoute<void>(builder: (context) =>
       //       SecondScreen(payload)),);
     }
+  }
+}
+
+
+
+
+/// Streams are created so that app can respond to notification-related events
+/// since the plugin is initialised in the `main` function
+final StreamController<ReceivedNotification> didReceiveLocalNotificationStream =
+StreamController<ReceivedNotification>.broadcast();
+
+final StreamController<String?> selectNotificationStream =
+StreamController<String?>.broadcast();
+
+
+
+class ReceivedNotification {
+  ReceivedNotification({
+    required this.id,
+    required this.title,
+    required this.body,
+    required this.payload,
+  });
+
+  final int id;
+  final String? title;
+  final String? body;
+  final String? payload;
+}
+
+
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key, required this.title});
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return  Placeholder(
+      child: Center(
+        child: Text(title),
+      ),
+    );
   }
 }
