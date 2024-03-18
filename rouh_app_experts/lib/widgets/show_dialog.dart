a98c66408bd69fc83e674be1bb1d6d172198f943
yasinidlbi <yasinidlbi@gmail.com>
2024-03-18 23:12:55 +0300

    Create show_dialog.dart

---
+++ rouh_app_experts/lib/widgets/show_dialog.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void ShowMessageDialog(BuildContext context, String title, String message) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        backgroundColor: Colors.white,
+        title: Text(
+          title,
+          style: TextStyle(fontSize: 18),
+        ),
+        content: Text(
+          message,
+          style: const TextStyle(fontSize: 16),
+        ),
+        actions: [
+          TextButton(
+            child: const Text('OK'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
