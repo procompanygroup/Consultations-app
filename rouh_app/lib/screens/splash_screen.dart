8fe71b478ddf717c05f829e7e8bca0e5bd8e81a4
yasinidlbi <yasinidlbi@gmail.com>
2024-04-18 23:40:24 +0300

    Update splash_screen.dart

--- rouh_app/lib/screens/splash_screen.dart
+++ rouh_app/lib/screens/splash_screen.dart
@@ -1,10 +1,15 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import '../controllers/globalController.dart';
+import '../models/country.dart';
 import 'login/login_screen.dart';
 import 'package:lottie/lottie.dart';
 import 'package:rouh_app/mystyle/constantsColors.dart';
 import 'package:video_player/video_player.dart';
+import 'package:http/http.dart' as http;
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -17,12 +22,20 @@ class _SplashScreenState extends State<SplashScreen>
     with SingleTickerProviderStateMixin {
   late VideoPlayerController _controller;
   late Future<void> _initializeVideoPlayerFuture;
+  bool isLoadingCountrys = true;
+  bool isLoadingVideo = true;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    initializeVideo();
+    fillGlobalCountryList();
 
+
+  }
+
+  Future<void> initializeVideo() async {
     // Create and store the VideoPlayerController. The VideoPlayerController
     // offers several different constructors to play videos from assets, files,
     // or the internet.
@@ -32,19 +45,48 @@ class _SplashScreenState extends State<SplashScreen>
 
     // Initialize the controller and store the Future for later use.
     _initializeVideoPlayerFuture = _controller.initialize();
+    setState(() {
+      isLoadingVideo =false;
+    });
+
     // Use the controller to loop the video.
     _controller.setLooping(false);
 
     _controller.play();
 
+
+
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
-    Future.delayed(Duration(seconds: 6), () {
+    Future.delayed(Duration(seconds: 6), () async {
+      while(isLoadingCountrys)
+        {
+          await Future.delayed(const Duration(seconds: 1));
+        };
       Navigator.of(context)
           .pushReplacement(MaterialPageRoute(builder: (_) => LoginScreen()));
-    });
-  }
 
-  @override
+    });
+
+
+  }
+  Future<void> fillGlobalCountryList() async {
+
+    globalCountryList =await Country.readJson();
+    // print("globalCountryList.first:" + globalCountryList.first.code.toString());
+      try {
+       var response = await http.get(Uri.parse('http://ip-api.com/json'));
+       globalCountryIp = json.decode(response.body)['countryCode'].toString();
+      } catch (err) {
+        globalCountryIp = '';
+      }
+    // print("globalCountryIp: " + globalCountryIp);
+      setState(() {
+        isLoadingCountrys =false;
+      });
+     print("isLoadingCountrys: " + isLoadingCountrys.toString());
+
+  }
+    @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
@@ -95,8 +137,15 @@ class _SplashScreenState extends State<SplashScreen>
       // color: myprimarycolor,
         width: double.infinity,
         height: MediaQuery.of(context).size.height,
-        color: Colors.white,
-        child: FutureBuilder(
+        // color: Colors.white,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/splash_background.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        // child: !isLoadingVideo? FutureBuilder(
+        child:  FutureBuilder(
           future: _initializeVideoPlayerFuture,
           builder: (context, snapshot) {
               return AspectRatio(
@@ -105,6 +154,7 @@ class _SplashScreenState extends State<SplashScreen>
                 child: VideoPlayer(_controller),
               );
           },
-        ),));
+        ),
+      ));
   }
 }
