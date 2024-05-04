f4f1b496e965ff1ccc4256dc849cfd457bf3f502
yasinidlbi <yasinidlbi@gmail.com>
2024-05-04 18:50:27 +0300

    Update custom_image_picker.dart

--- rouh_app/lib/components/custom_image_picker.dart
+++ rouh_app/lib/components/custom_image_picker.dart
@@ -23,93 +23,84 @@ class _ImagePickerState extends State<CustomImagePicker> {
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     String imagePath = "";
-    return GestureDetector(
-          onTap: () {
-            print(imagePath);
-            getImagefromGallery();
-          },
-          child: Container(
-            height: screenWidth,
-            width: screenWidth,
-            child: Stack(
-              children: [
-                // imagePath ==  File('')?
-                !uploading?
-                // background icon
-                     Container(
-                  decoration:  BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      border: Border.all(color: Colors.grey.shade200, width: 1),
-                      color: Colors.grey.shade100
-                  ),
-                  child: Positioned(
-                    left: 0,
-                    top: 0,
-                    right: 0,
-                    bottom: 0,
-                    child: Center(
-                      child: SvgPicture.asset(
-                        'assets/svg/plus-icon.svg',
-                        height: 35,
-                        width: 35,
-                        fit: BoxFit.cover,
-                        color: Colors.grey.shade400,
-                        // color: Colors.red,
-                      ),
-                    ),
-                  ),
-                )
-                    :
-                Container(
-                  width: screenWidth,
-                  height: screenWidth,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      border: Border.all(color: Colors.grey.shade200, width: 1),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.03),
-                          spreadRadius: 5,
-                          blurRadius: 7,
-                          offset: Offset(0, 3), // changes position of shadow
+    return Container(
+      // color: Colors.pink,
+      height: screenWidth,
+      width: screenWidth,
+      child: GestureDetector(
+            onTap: () {
+              print(imagePath);
+              getImagefromGallery();
+            },
+            child: Container(
+              height: screenWidth,
+              width: screenWidth,
+              child:Builder(
+                    builder: (context) {
+                      if(!uploading)
+                      // background icon
+                      return  Container(
+                        height: screenWidth,
+                        width: screenWidth,
+                        decoration:  BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(color: Colors.grey.shade200, width: 1),
+                            // color: Colors.grey.shade100
+                            // color: Colors.purple
                         ),
-                      ],
-                      color: Colors.grey.shade100),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Image(
-                      // image: NetworkImage(imagePath),
-                      image: FileImage(image),
-                      fit: BoxFit.cover,
-                      errorBuilder:
-                          (BuildContext context, Object exception, StackTrace? stackTrace) {
-                        return Image(
-                          //image: FileImage(image),
-                          image: AssetImage("assets/images/default_image.png"),
-                          fit: BoxFit.cover,
-                        );
-                      },
-                    ),
-                    /*
-                    Image(
-                      image:NetworkImage("https://picsum.photos/200/300?random=4"),
-                      fit: BoxFit.cover,
-                      errorBuilder:
-                          (BuildContext context, Object exception, StackTrace? stackTrace) {
-                        return Image(
-                          image: AssetImage("assets/images/default_image.png"),
-                          fit: BoxFit.fitHeight,
-                        );
-                      },
-                    ),
-                    */
-                  ),
-                ),
+                        child: Center(
+                          child: Container(
+                            width: 35,
+                            height: 35,
+                            child:  SvgPicture.asset(
+                              'assets/svg/plus-icon.svg',
+                              height: 35,
+                              width: 35,
+                              fit: BoxFit.cover,
+                              color: Colors.grey.shade400,
+                            ),
+                          ),
+                        ),
+                      );
+                      else
+                      return   Container(
+                        width: screenWidth,
+                        height: screenWidth,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(color: Colors.grey.shade200, width: 1),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.03),
+                                spreadRadius: 5,
+                                blurRadius: 7,
+                                offset: Offset(0, 3), // changes position of shadow
+                              ),
+                            ],
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image(
+                            // image: NetworkImage(imagePath),
+                            image: FileImage(image),
+                            fit: BoxFit.cover,
+                            errorBuilder:
+                                (BuildContext context, Object exception, StackTrace? stackTrace) {
+                              return Image(
+                                //image: FileImage(image),
+                                image: AssetImage("assets/images/default_image.png"),
+                                fit: BoxFit.cover,
+                              );
+                            },
+                          ),
 
-              ],
+                        ),
+                      );
+                    },
+                  ),
             ),
           ),
-        );
+    );
   }
 
 getImagefromGallery() async {
