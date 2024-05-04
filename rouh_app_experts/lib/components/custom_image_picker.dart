e12e24618a4164b8e961af94df3376d1cc8fbd63
yasinidlbi <yasinidlbi@gmail.com>
2024-05-04 18:50:54 +0300

    Update custom_image_picker.dart

--- rouh_app_experts/lib/components/custom_image_picker.dart
+++ rouh_app_experts/lib/components/custom_image_picker.dart
@@ -1,11 +1,14 @@
+/*
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class CustomImagePicker extends StatefulWidget {
-  const CustomImagePicker({super.key});
 
+class CustomImagePicker extends StatefulWidget {
+  const CustomImagePicker({super.key, required this.ImageInputServiceId,required this.index});
+  final int ImageInputServiceId;
+  final int index;
   @override
   State<CustomImagePicker> createState() => _ImagePickerState();
 }
@@ -20,58 +23,61 @@ class _ImagePickerState extends State<CustomImagePicker> {
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     String imagePath = "";
-    return GestureDetector(
-          onTap: () {
-            print(imagePath);
-            getImagefromGallery();
-          },
-          child: Container(
-            height: screenWidth,
-            width: screenWidth,
-            child: Stack(
-              children: [
-                // imagePath ==  File('')?
-                !uploading?
+    return Container(
+      // color: Colors.pink,
+      height: screenWidth,
+      width: screenWidth,
+      child: GestureDetector(
+        onTap: () {
+          print(imagePath);
+          getImagefromGallery();
+        },
+        child: Container(
+          height: screenWidth,
+          width: screenWidth,
+          child:Builder(
+            builder: (context) {
+              if(!uploading)
                 // background icon
-                     Container(
+                return  Container(
+                  height: screenWidth,
+                  width: screenWidth,
                   decoration:  BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      border: Border.all(color: Colors.grey.shade200, width: 1),
-                      color: Colors.grey.shade100
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.grey.shade200, width: 1),
+                    // color: Colors.grey.shade100
+                    // color: Colors.purple
                   ),
-                  child: Positioned(
-                    left: 0,
-                    top: 0,
-                    right: 0,
-                    bottom: 0,
-                    child: Center(
-                      child: SvgPicture.asset(
+                  child: Center(
+                    child: Container(
+                      width: 35,
+                      height: 35,
+                      child:  SvgPicture.asset(
                         'assets/svg/plus-icon.svg',
                         height: 35,
                         width: 35,
                         fit: BoxFit.cover,
                         color: Colors.grey.shade400,
-                        // color: Colors.red,
                       ),
                     ),
                   ),
-                )
-                    :
-                Container(
+                );
+              else
+                return   Container(
                   width: screenWidth,
                   height: screenWidth,
                   decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      border: Border.all(color: Colors.grey.shade200, width: 1),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.03),
-                          spreadRadius: 5,
-                          blurRadius: 7,
-                          offset: Offset(0, 3), // changes position of shadow
-                        ),
-                      ],
-                      color: Colors.grey.shade100),
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.grey.shade200, width: 1),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.03),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(20),
                     child: Image(
@@ -87,39 +93,29 @@ class _ImagePickerState extends State<CustomImagePicker> {
                         );
                       },
                     ),
-                    /*
-                    Image(
-                      image:NetworkImage("https://picsum.photos/200/300?random=4"),
-                      fit: BoxFit.cover,
-                      errorBuilder:
-                          (BuildContext context, Object exception, StackTrace? stackTrace) {
-                        return Image(
-                          image: AssetImage("assets/images/default_image.png"),
-                          fit: BoxFit.fitHeight,
-                        );
-                      },
-                    ),
-                    */
+
                   ),
-                ),
-
-              ],
-            ),
+                );
+            },
           ),
-        );
+        ),
+      ),
+    );
   }
 
-getImagefromGallery() async {
-  //final status = await ImagePicker();
-  final pickedfile = await picker.pickImage(source: ImageSource.gallery);
-  if (pickedfile != null) {
-    setState(() {
-      image = File(pickedfile.path);
-      imagePath = pickedfile.path;
-      print(imagePath);
-      uploading = true;
-    });
+  getImagefromGallery() async {
+    //final status = await ImagePicker();
+    final pickedfile = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedfile != null) {
+      setState(() {
+        image = File(pickedfile.path);
+        imagePath = pickedfile.path;
+        print(imagePath);
+        serviceImages[widget.index] = imagePath;
+        uploading = true;
+      });
 
+    }
   }
 }
-}
+*/
