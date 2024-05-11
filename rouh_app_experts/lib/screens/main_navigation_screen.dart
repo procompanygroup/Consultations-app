70a67afe784d14faccc738cc08e922794ffde65f
yasinidlbi <yasinidlbi@gmail.com>
2024-05-11 12:45:20 +0300

    Update main_navigation_screen.dart

--- rouh_app_experts/lib/screens/main_navigation_screen.dart
+++ rouh_app_experts/lib/screens/main_navigation_screen.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rouh_app_experts/mystyle/constantsColors.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:rouh_app_experts/screens/orders/main_orders_screen.dart';
 
+import '../controllers/globalController.dart';
+import '../controllers/local_notification_service.dart';
+import '../models/firebase_notificaton_model.dart';
 import 'experts/experts_screen.dart';
 import 'profile/profile_screen.dart';
 import 'settings/settings_screen.dart';
@@ -16,10 +21,121 @@ class MainNavigationScreen extends StatefulWidget {
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
   int _selectedIndex = 0;
 
+  void _configureDidReceiveLocalNotificationSubject() {
+    didReceiveLocalNotificationStream.stream
+        .listen((ReceivedNotification receivedNotification) async {
+      await showDialog(
+        context: context,
+        builder: (BuildContext context) => CupertinoAlertDialog(
+          title: receivedNotification.title != null
+              ? Text(receivedNotification.title!)
+              : null,
+          content: receivedNotification.body != null
+              ? Text(receivedNotification.body!)
+              : null,
+          actions: <Widget>[
+            CupertinoDialogAction(
+              isDefaultAction: true,
+              onPressed: () async {
+                Navigator.of(context, rootNavigator: true).pop();
+
+                // await Navigator.of(context).push(
+                //   MaterialPageRoute<void>(
+                //     builder: (BuildContext context) =>
+                //         SecondPage(title:  receivedNotification.payload!),
+                //   ),
+                // );
+
+                await globalLocalNotificationService.NotificationForwardPage(receivedNotification.payload!,context);
+
+
+              },
+              child: const Text('Ok'),
+            )
+          ],
+        ),
+      );
+    });
+  }
+  void _configureSelectNotificationSubject() {
+    selectNotificationStream.stream.listen((String? payload) async {
+
+      // await Navigator.of(context).push(MaterialPageRoute<void>(
+      //   builder: (BuildContext context) => SecondPage(title: payload!),
+      // ));
+      await globalLocalNotificationService.NotificationForwardPage(payload,context);
+
+    });
+  }
+
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
+  }
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _configureDidReceiveLocalNotificationSubject();
+    _configureSelectNotificationSubject();
+
+    FirebaseMessagingListenForground();
+/*
+    static final AndroidInitializationSettings initializationSettingsAndroid =
+    AndroidInitializationSettings('mipmap/ic_launcher');
+
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+        AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(androidChannel);
+    flutterLocalNotificationsPlugin.initialize(
+      InitializationSettings(android: initializationSettingsAndroid, iOS: null),
+    );
+
+    */
+    super.initState();
+
+
+  }
+
+  void FirebaseMessagingListenForground()
+  {
+    try{
+      FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+        print(message);
+        print('Handling a forground message ${message.messageId}');
+        print(message.data);
+
+        FirebaseNotificatonModel firebaseNotificaton = FirebaseNotificatonModel();
+        firebaseNotificaton = FirebaseNotificatonModel.fromJson(message.data);
+        print('firebaseNoti');
+        print(firebaseNotificaton);
+        print('firebaseNoti.id');
+        print(firebaseNotificaton.id);
+        print(firebaseNotificaton.title);
+        print(firebaseNotificaton.body);
+
+        globalLocalNotificationService.showNotification(title: firebaseNotificaton.title, body: firebaseNotificaton.body ,payload: firebaseNotificaton.id.toString());
+
+      });
+    } catch (err) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(err.toString()),
+          )
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    didReceiveLocalNotificationStream.close();
+    selectNotificationStream.close();
+
+    super.dispose();
   }
 
   @override
