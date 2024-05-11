df716fef6f552943703f0b1b7d99c08bf5c7db2b
yasinidlbi <yasinidlbi@gmail.com>
2024-05-11 12:44:55 +0300

    Update main.dart

--- rouh_app_experts/lib/main.dart
+++ rouh_app_experts/lib/main.dart
@@ -1,14 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rouh_app_experts/bloc/audio_file/audio_file_cubit.dart';
 
 import 'bloc/expert/expert_information_cubit.dart';
+import 'controllers/globalController.dart';
+import 'firebase_options.dart';
+import 'models/firebase_notificaton_model.dart';
 import 'mystyle/constantsColors.dart';
 import 'screens/main_navigation_screen.dart';
 import 'screens/splash_screen.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await globalLocalNotificationService.initNotification();
+
   // runApp(const MyApp());
 
   runApp(MultiBlocProvider(
@@ -23,7 +31,45 @@ void main() async {
     ],
     child: MyApp(),
   ), );
+
+  FirebaseMessagingListenBackground();
 }
+
+
+Future<void> FirebaseMessagingListenBackground()
+async {
+  try{
+
+
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+    // FirebaseMessaging.;print forgound
+
+  } catch (err) {
+    print(err.toString());
+  }
+}
+
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('Handling a background message ${message.messageId}');
+  print(message.data);
+
+
+  FirebaseNotificatonModel firebaseNotificaton = FirebaseNotificatonModel();
+  firebaseNotificaton = FirebaseNotificatonModel.fromJson(message.data);
+  print('firebaseNoti');
+  print(firebaseNotificaton);
+  print('firebaseNoti.id');
+  print(firebaseNotificaton.id);
+
+  globalLocalNotificationService.showNotification(title: firebaseNotificaton.title, body: firebaseNotificaton.body ,payload: firebaseNotificaton.id.toString());
+
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
