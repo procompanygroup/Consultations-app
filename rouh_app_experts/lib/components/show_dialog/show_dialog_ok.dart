91224f09fb5f4fdb59e966893d2e70b79f6920a6
yasinidlbi <yasinidlbi@gmail.com>
2024-05-24 00:27:28 +0300

    fix dialog proplem

--- rouh_app_experts/lib/components/show_dialog/show_dialog_ok.dart
+++ rouh_app_experts/lib/components/show_dialog/show_dialog_ok.dart
@@ -1,57 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:rouh_app_experts/mystyle/constantsColors.dart';
 
 import '../../mystyle/button_style.dart';
-import '../../mystyle/constantsColors.dart';
 
 Future ShowDialogOk(BuildContext context,double _height, double _width,  String body,String ok, Function() okFunction) {
   return showDialog(context: context, builder: (BuildContext context) =>
       Align(
         alignment: Alignment.center,
         child: Container(
-          height:_height + 50 ,
-          width:_width ,
-          child: Stack(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 50),
-                // child: Dialog(
-                //   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)), //this right here
-                child: Container(
-                  // height: 300.0,
-                  // width: 300.0,
-                  height:_height,
-                  width: _width,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      color: Colors.white),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      SizedBox(height: 30,),
-                      Expanded(
-                        child: SingleChildScrollView(
-                          scrollDirection: Axis.vertical,
-                          child: Padding(
-                            padding:  EdgeInsets.all(10.0),
-                            child: Container(
-                              child: Center(
-                                child: Text(body,
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                      color: myprimercolor,
-                                      fontSize: 22
-                                  ),),
+              // height: 300.0,
+              // width: 300.0,
+              height:_height,
+              width: _width,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.white),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  // height: 30
+                  SizedBox(height: 30,),
+                  // height: _height - 30-10-70-10
+                  Container(
+                    height: _height - 30-10-70-10,
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: SingleChildScrollView(
+                            scrollDirection: Axis.vertical,
+                            child: Padding(
+                              padding:  EdgeInsets.all(10.0),
+                              child: Container(
+                                child: Center(
+                                  child: Text(body,
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        color: myprimercolor,
+                                        fontSize: 22
+                                    ),),
+                                ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                      SizedBox(height: 10,),
-                      Container(
-                        width: _width,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                          child: Expanded(
+                      ],
+                    ),
+                  ),
+                  // height: 10
+                  SizedBox(height: 10,),
+                  // height: 70
+                  Container(
+                    height: 70,
+                    width: _width,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                      child: Row(
+                        children: [
+                          Expanded(
                             child: Row(
                               children: [
                                 Container(
@@ -74,19 +79,16 @@ Future ShowDialogOk(BuildContext context,double _height, double _width,  String 
                               ],
                             ),
                           ),
-                        ),
+                        ],
                       ),
-                      SizedBox(height: 10,)
-
-                    ],
+                    ),
                   ),
-                ),
-                // ),
-              ),
+                  // height: 10
+                  SizedBox(height: 10,)
 
-            ],
-          ),
-        ),
+                ],
+              ),
+            ),
       ),
   );
 }
